af81e8a8168ddea49b9566bd8ac95f391763b2f1
Thomas <janast@berkeley.edu>
2025-01-31 12:38:09 -0600

    Added many todos, solved some precision when calculating LJ_potential

--- assignment1/h1.cpp
+++ assignment1/h1.cpp
@@ -3,37 +3,56 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <exception>
+#include <cmath>
 
 
-// TODO Raise an error if the atomic number is not 79 (Gold/Au) - use std::exception
+// TODO: Raise an error if the atomic number is not 79 (Gold/Au) - use std::exception
 // Echo the input
+
 class Atom
 {
     private:
         int atomic_number;
         std::string bond_units = "angstroms";
-        // TODO Use eigen or armadillo for coordinates, but we will worry about that later
-        int x;
-        int y;
-        int z;
+        double sigma;
     
     public:
+        // TODO Use eigen or armadillo for coordinates, but we will worry about that later
+        double x;
+        double y;
+        double z;
+
         Atom(int atomic_number_, int x_, int y_, int z_) :
-            atomic_number(atomic_number_), x(x_), y(y_), z(z_) {}
+            atomic_number(atomic_number_), x(x_), y(y_), z(z_)
+            {
+                if (atomic_number_ != 79) // Error handles non-gold. TODO handle < 1 cases
+                {
+                    throw std::exception();
+                }
+                sigma = 0.26290;
+            }
         
         int get_atomic_number() const { atomic_number; }
 
         // TODO Change to armadillo vector
         void get_coords() const {}
 
-        void set_atomic_number()
-        {
+        double get_sigma() const { sigma; }
 
+        void set_atomic_number(int new_atomic_number)
+        {
+            atomic_number = new_atomic_number;
         }
 
         void set_coords()
         {
 
+        }
+
+        Atom operator-(Atom atom_2)
+        {
+            x - atom_2.x;
         }
 };
 
@@ -45,7 +64,9 @@ class Cluster
         int num_atoms = 0;
 
     public:
-        void add_atom(int atomic_number, int x, int y, int z)
+
+        // TODO: Find out if I have to template this or if I can just take in an integer, float, double etc.
+        void add_atom(int atomic_number, double x, double y, double z)
         {
             Atom new_atom(atomic_number, x, y, z);
             atoms.push_back(new_atom);
@@ -76,6 +97,12 @@ class Cluster
 Cluster load_atoms(std::string file)
 {
     std::ifstream inputFile(file);
+    if (!inputFile)
+    {
+        // TODO: Output error message
+        // Incorrect or corrupt file
+        throw std::exception();
+    }
     std::string line;
     getline(inputFile, line);
 
@@ -94,9 +121,9 @@ Cluster load_atoms(std::string file)
         std::cout << line << std::endl;
 
         int atomic_number;
-        int x;
-        int y;
-        int z;
+        double x;
+        double y;
+        double z;
         
         std::istringstream numbers(line);
         numbers >> atomic_number;
@@ -111,25 +138,46 @@ Cluster load_atoms(std::string file)
     return cluster_1;
 }
 
-int calculate_distance(int coord_1, int coord_2)
+// TODO: Can I do const correctness for these functions?
+double calculate_distance(int coord_1, int coord_2)
 {
-    return 0;
+    return 1;
 }
 
-int calculate_distance(Atom atom_1, Atom atom_2)
+double calculate_distance(Atom atom_1, Atom atom_2)
 {
-    return 0;
+    double x = atom_1.x - atom_2.x;
+    double y = atom_1.y - atom_2.y;
+    double z = atom_1.z - atom_2.z;
+
+    double dist = std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
+    std::cout << "printing distance: " << dist << std::endl;
+    return dist;
 }
 
-int calculate_energy(Cluster clusters)
+double calculate_energy(Cluster clusters)
 {
-    
-    for (auto& atom : clusters.get_atoms())
+    // Sum of pair-wise energies using sigma_ij/radius_ij
+    int num_atoms = clusters.get_num_atoms();
+    double total_energy = 0;
+
+    for (int i = 0; i < num_atoms - 1; i++)
     {
-        // calculate_distance(atom);
+        for (int j = i + 1; j < num_atoms; j++)
+        {
+            double sigma_ij = 0.2629;
+            double radius_ij = calculate_distance(clusters.get_atoms()[i], clusters.get_atoms()[j]);
+            total_energy += std::pow((sigma_ij/radius_ij), 6) - 2 * std::pow((sigma_ij/radius_ij), 12);
+            std::cout << total_energy << std::endl;
+        }
     }
     
-    return 0;
+    // for (auto& atom : clusters.get_atoms())
+    // {
+    //     calculate_distance(atom);
+    // }
+    
+    return total_energy;
 }
 
 int main(void)
@@ -138,7 +186,7 @@ int main(void)
     // gold.print_atoms();
 
     // 1. Calculate Energy
-    int energy_of_the_cluster = 0; // calculate_energy();
+    double energy_of_the_cluster = calculate_energy(gold); // calculate_energy();
     std::cout << "E_LJ = " << energy_of_the_cluster << std::endl;
 
     // 2. Calculate Force
@@ -147,5 +195,5 @@ int main(void)
 
 
     std::cout << "Done" << std::endl;
-    return energy_of_the_cluster;
+    return 0;
 }
