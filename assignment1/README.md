f6252b13db9e17813a27db6346ef57236f57a3fb
Thomas <janast@berkeley.edu>
2025-02-07 12:00:26 -0600

    Fixing unstaged issues

---
+++ assignment1/README.md
@@ -0,0 +1,5 @@
+# Inital Tasks
+1. Develop 1 class file for holding useful classes
+2. Develop 1 utility file for holding useful functions
+    - This includes loading atoms into classes
+3. Develop 1 main file for each assignment part
---
+++ assignment1/build/CMakeFiles/h1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/thomasjanas/Documents/programming headquarters/berkeley/CHEM_279/Assignments/numerical_algorithms_applied_to_computational_quantum_chemistry/assignment1/h1.cpp" "CMakeFiles/h1.dir/h1.cpp.o" "gcc" "CMakeFiles/h1.dir/h1.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
